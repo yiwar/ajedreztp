b2452256f9ce487fc87e356466d464e80963926f
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-10 18:23:38 +0000

    Implementada clase principal del ajedrez.

---
+++ src/Ajedrez.java
@@ -0,0 +1,15 @@
+/*
+ *$LastChangedDate$
+ *$LastChangedRevision$
+ * Ligia Tatiana Gonzalez Leyva <calvinahobbes@gmail.com>
+ */
+
+import java.io.*;
+
+public class Ajedrez {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Refresco de memoria");
+	}
+}
