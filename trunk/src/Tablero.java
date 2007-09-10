edab1e5bc2c125f54778e0169de28016401cef0c
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-10 19:19:38 +0000

    Primera aproximación del tablero.

---
+++ trunk/src/Tablero.java
@@ -0,0 +1,16 @@
+/*
+ *$LastChangedDate$
+ *$LastChangedRevision$
+ * Ligia Tatiana Gonzalez Leyva <calvinahobbes@gmail.com>
+ */
+
+import java.io.*;
+
+public class Tablero {
+
+	// Constructor
+	public Tablero () {
+		System.out.println("Tablero.Tablero(): Me acaban de construir");
+	}
+}
+
