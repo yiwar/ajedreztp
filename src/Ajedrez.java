f590d10fbd54624db0e0b9b6281edc7a839f2372
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-10 19:34:34 +0000

    Añadidos Jugador.java y JugadorHumano.java

--- src/Ajedrez.java
+++ src/Ajedrez.java
@@ -11,8 +11,10 @@ public class Ajedrez {
 	public static void main(String[] args) {
 	
 		Tablero t;
+		JugadorHumano jh;
 		System.out.println("Ajedrez.main(): He empezado");
 
 		t = new Tablero();
+		jh = new JugadorHumano();
 	}
 }
