21e060563e32aedceffcc293f3bf0b39c4cc6140
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-10 19:34:34 +0000

    Añadidos Jugador.java y JugadorHumano.java

---
+++ trunk/src/Jugador.java
@@ -0,0 +1,15 @@
+/*
+ *$LastChangedDate$
+ *$LastChangedRevision$
+ * Ligia Tatiana Gonzalez Leyva <calvinahobbes@gmail.com>
+ */
+
+import java.io.*;
+
+public abstract class Jugador {
+	
+	// constructor
+	public Jugador () {
+		System.out.println("Jugador.Jugador(): Me acaban de crear");
+	}
+}	
---
+++ trunk/src/JugadorHumano.java
@@ -0,0 +1,17 @@
+/*
+ *$LastChangedDate$
+ *$LastChangedRevision$
+ * Ligia Tatiana Gonzalez Leyva <calvinahobbes@gmail.com>
+ */
+
+import java.io.*;
+
+public class JugadorHumano extends Jugador {
+
+	// Constructor
+	public JugadorHumano () {
+		
+		System.out.println("JugadorHumano.JugadorHumano(): Me acaban de crear");
+	}
+}
+
