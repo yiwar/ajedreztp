fdbc9e5626eedc5626173e8efcb2074ed181ce5d
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-12 16:11:28 +0000

    Actualizado script run.bat

--- trunk/src/run.bat
+++ trunk/src/run.bat
@@ -1,7 +1,12 @@
+@ECHO OFF
 @REM $LastChangedDate$
 @REM $LastChangedRevision$
-@REM  Ligia Tatiana Gonzalez Leyva <calvinahobbes@gmail.com>
-
+ECHO **********************************************************
+ECHO * Ligia Tatiana Gonzalez Leyva (calvinahobbes@gmail.com) *
+ECHO *       Tecnologia de la Programacion 2006/2007          *
+ECHO *                EL JUEGO DEL AJEDREZ                    *
+ECHO **********************************************************
+ECHO.
 @REM Script MS-DOS para ejecutar la aplicación Ajedrez.java
 @REM Si el compilador javac está en el path la línea 1 puede ser omitida.
 @REM Puede que en diferentes instalaciones sea necesario modificar susodicha línea.
