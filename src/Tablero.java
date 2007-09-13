685045bbaf73b0a29bac2f834c1253561843c80f
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-13 19:36:18 +0000

    Creado tablero gráfico.

--- src/Tablero.java
+++ src/Tablero.java
@@ -5,16 +5,32 @@
  */
 
 import java.io.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Tablero {
 
+	private JPanel tableroJP;
 	private Pieza[][] tablero;
+	private JPanel[][] casillasJP;
 
 	// Constructor
 	public Tablero () {
 		
 		tablero = new Pieza[8][8];
+		casillasJP = new JPanel[8][8];
 		System.out.println("Tablero.Tablero(): Me acaban de crear");
+	}
+
+	// version con GUI
+	public Tablero (JFrame f) {
+
+		this();
+		Container c = f.getContentPane();
+		tableroJP = new JPanel();
+		c.add(tableroJP, "Center");
 	}
 
 	public void add (int x, int y, Pieza p) {
@@ -36,5 +52,50 @@ public class Tablero {
 			System.out.println();
 		}
 	}
+
+	public void crearTableroGUI (JFrame f) {
+
+        	tableroJP.setLayout(new GridLayout(10, 10));
+        	filaLetras(tableroJP);
+        	for (int i = 0; i < 8; i++) {
+            		tableroJP.add(new BLabel((new Integer(i+1)).toString()));
+            		for (int j = 0; j < 8; j++) {
+				JPanel jp = new JPanel();
+				if (((i + j) % 2) == 0) jp.setBackground(Color.white);
+       				else jp.setBackground(Color.gray);
+				jp.setPreferredSize(new Dimension(42, 42));
+                		JLabel jl = new JLabel();
+        			jl.setPreferredSize(new Dimension(32, 32));
+        			jp.add(jl);
+				casillasJP[i][j] = jp;
+
+                		//int color = b.getColor(i, j);
+                		//if (color != Board.EMPTY) {
+                    		//	int piece = b.getPiece(i, j);
+                    		//	square[i][j].setIcon(new ImageIcon(pieceImage[color][piece]));
+                		//}
+                		tableroJP.add(casillasJP[i][j]);
+            		}
+            		tableroJP.add(new BLabel((new Integer(i+1)).toString()));
+        	}
+        	filaLetras(tableroJP);
+	}
+
+	
+	// Añade una fila de letras al tablero
+	private void filaLetras(JPanel p) {
+        	p.add(new JPanel());
+        	for (int i = 0; i < 8; i++)
+            		p.add(new BLabel((new Character((char) ('a' + i))).toString()));
+        	p.add(new JPanel());
+    	}
+
+    	class BLabel extends JLabel {
+        	BLabel(String s) {
+            		super(s);
+            		setHorizontalAlignment(CENTER);
+            		setVerticalAlignment(CENTER);
+		}
+        }
 }
 
