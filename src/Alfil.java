0b9d125a8cc6fb8b5b2f59bd63a927d9b831aef4
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-16 18:33:24 +0000

    Actualizada la validación de movimientos para el alfíl.

--- src/Alfil.java
+++ src/Alfil.java
@@ -25,7 +25,36 @@ public class Alfil extends Pieza {
 
 	public boolean validarMovimiento (Tablero tablero, int fF, int cF) {
 		
-		return true;
+		// diferencia entre filas y columnas para el movimiento
+		int difFil, difCol;
+
+		Pieza p;
+
+		p = tablero.get(fF, cF);
+
+		difFil = Math.abs(this.f - fF);
+		difCol = Math.abs(this.c - cF);
+		
+		//un valor igual para ambas diferencias indica un moviento en diagonal
+		if ( (difFil == difCol) ) {
+			// la casilla destino no esta vacia
+			if (p != null )
+				//no se puede comer una pieza de igual color
+				if (p.getColor() == this.color)
+					return false;
+
+			// el alfil no puede saltar a otras fichas
+			for ( int i = 1; i < difFil; i++ ) {
+				p = tablero.get( this.f + i * ((fF - this.f) / difFil), this.c + i * ((cF - this.c) / difFil));
+				// trata de saltar a otra ficha
+				if (p != null )
+					return false;
+			}
+			return true;
+		}
+		// no es un movimiento en diagonal
+		else 
+			return false;
 	}
 }
 
