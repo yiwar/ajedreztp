e19f40ba62a50d23f4cfd4726ac4ba9bae501edf
vf.dalmau <vf.dalmau@510fded7-c839-0410-bf3a-ab6474c13fa3>
2007-09-17 16:09:01 +0000

    Actualizada la validación de movimientos para la torre.

--- src/Torre.java
+++ src/Torre.java
@@ -25,7 +25,33 @@ public class Torre extends Pieza {
 
 	public boolean validarMovimiento (Tablero tablero, int fF, int cF) {
 
-		return true;
+		int difFil, difCol, diferencia;
+		Pieza p;
+
+		difFil = fF - this.f;
+		difCol = cF - this.c;
+
+		p = tablero.get(fF, cF);
+
+		//se debe mover sobre la misma fila o sobre la misma columna
+		if ((this.f == fF) || (this.c == cF)) {
+			// solo podemos comer una ficha del enemigo
+			if (p != null )
+				if (p.getColor() == this.color)
+					return false;
+			
+			// la torre no puede saltar sobre otras fichas
+			diferencia = Math.abs(difFil) + Math.abs(difCol);
+			for (int i = 1; i < diferencia; i++) {	
+				p = tablero.get(this.f + i * (difFil / diferencia), this.c + i * (difCol / diferencia));
+				// la torre trata de saltar
+				if (p != null )
+					return false;
+			}
+			return true;
+		}
+		else 
+			return false;
 	}
 }
 
